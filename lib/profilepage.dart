6f40d1ed9470b70df5d39b7219a6ce5ee498d916
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-12-14 06:20:37 +0700

    update profilepage: add controller and enhance style

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -25,6 +25,7 @@ class _ProfilePageState extends State<ProfilePage> {
         appBar: AppBar(
           title: const Text("Profile Page"),
           backgroundColor: appThemeController.currentColor.value,
+          elevation: 0,
         ),
         body: Container(
           alignment: Alignment.center,
@@ -40,10 +41,81 @@ class _ProfilePageState extends State<ProfilePage> {
                 height: screenUtil.setHeight(10),
               ),
               Text("Angka Saat Ini: ${increDecreController.count1.value}"),
+              SizedBox(
+                height: screenUtil.setHeight(10),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  profileButton(
+                    child: const Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        increDecreController.increment1();
+                        increDecreController.increment2();
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    width: screenUtil.setWidth(10),
+                  ),
+                  profileButton(
+                    child: const Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        increDecreController.decrement1();
+                        increDecreController.decrement2();
+                      });
+                    },
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       increDecreController.increment1();
+                  //       increDecreController.increment2();
+                  //     });
+                  //   },
+                  //   child: const Icon(Icons.add),
+                  //   style: ElevatedButton.styleFrom(
+                  //     primary: appThemeController.currentColor.value,
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   width: screenUtil.setWidth(10),
+                  // ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       increDecreController.decrement1();
+                  //       increDecreController.decrement2();
+                  //     });
+                  //   },
+                  //   child: const Icon(Icons.remove),
+                  //   style: ElevatedButton.styleFrom(
+                  //     primary: appThemeController.currentColor.value,
+                  //   ),
+                  // ),
+                ],
+              ),
             ],
           ),
         ),
       );
     });
+  }
+
+  Widget profileButton({Widget? child, void Function()? onPressed}) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: child,
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        // minimumSize: Size(screenUtil.setWidth(40), screenUtil.setHeight(40)),
+        // shape: const CircleBorder(),
+        primary: appThemeController.currentColor.value,
+      ),
+    );
   }
 }
